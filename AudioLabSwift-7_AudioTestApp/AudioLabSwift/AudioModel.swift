929c85f1004439407861c5ab3b6c9a54281b4f07
Jeevan Rai <yamphurai007@yahoo.com>
2024-10-03 22:36:47 -0500

    Cleaned up the template code

--- AudioLabSwift-7_AudioTestApp/AudioLabSwift/AudioModel.swift
+++ AudioLabSwift-7_AudioTestApp/AudioLabSwift/AudioModel.swift
@@ -1,98 +1,121 @@
-//
-//  AudioModel.swift
-//  AudioLabSwift
-//
-//  Created by Eric Larson 
+//  Created by Eric Larson
 //  Copyright © 2020 Eric Larson. All rights reserved.
-//
+
+// To manage collection & manipulation of audio data, handle microphone input, compute FFT, and audio playback
+
 
 import Foundation
 import Accelerate
 
 class AudioModel {
     
-    // MARK: Properties
-    private var BUFFER_SIZE:Int
-    // thse properties are for interfaceing with the API
-    // the user can access these arrays at any time and plot them if they like
-    var timeData:[Float]
-    var fftData:[Float]
-    lazy var samplingRate:Int = {
-        return Int(self.audioManager!.samplingRate)
-    }()
+    // MARK: Private Properties
+    private var BUFFER_SIZE:Int    // size of the buffer to store audio data
     
+    // phase 1, 2 & 3 are initial phases for sine wave generation
+    private var phase1:Float = 0.0
+    private var phase2:Float = 0.0
+    private var phase3:Float = 0.0
+    
+    // phase increments for since wave generation
+    private var phaseIncrement1:Float = 0.0
+    private var phaseIncrement2:Float = 0.0
+    private var phaseIncrement3:Float = 0.0
+    
+    private var sineWaveRepeatMax:Float = Float(2*Double.pi)   //max value for phase before it resets
+    private var pulseValue:Int = 0     //to track pulse state
+    
+
+    // MARK: Public Properties
+    var timeData:[Float]                 // array to store time-domain audio data
+    var fftData:[Float]                  // array to store fft data of "timeData"
+    
+    // frequencies for generating sine waves
     var sineFrequency1:Float = 300.0
     var sineFrequency2:Float = 650.0
     var sineFrequency3:Float = 1000.0
     
-    private var phase1:Float = 0.0
-    private var phase2:Float = 0.0
-    private var phase3:Float = 0.0
-    private var phaseIncrement1:Float = 0.0
-    private var phaseIncrement2:Float = 0.0
-    private var phaseIncrement3:Float = 0.0
-    private var sineWaveRepeatMax:Float = Float(2*Double.pi)
-    var pulsing:Bool = false
-    private var pulseValue:Int = 0
+    var pulsing:Bool = false    // to determine if pulsing effect is active
+    
+    // property "samplingRate" to get sampling rate from "audioManager"
+    lazy var samplingRate:Int = {
+        return Int(self.audioManager!.samplingRate)
+    }()
+    
+
     
     // MARK: Public Methods
+    
+    // Initialize "AudioModel" with specific buffer size
     init(buffer_size:Int) {
-        BUFFER_SIZE = buffer_size
-        // anything not lazily instatntiated should be allocated here
-        timeData = Array.init(repeating: 0.0, count: BUFFER_SIZE)
-        fftData = Array.init(repeating: 0.0, count: BUFFER_SIZE/2)
+        BUFFER_SIZE = buffer_size    //size of the frequency buffer
+        timeData = Array.init(repeating: 0.0, count: BUFFER_SIZE)   //allocate memory for "timeData" array
+        fftData = Array.init(repeating: 0.0, count: BUFFER_SIZE/2)  //allocate memory for "fftData" array
     }
+    
     
     // public function for starting processing of microphone data
     func startMicrophoneProcessing(withFps:Double){
-        // setup the microphone to copy to circualr buffer
+        
+        // If audioManager is availabe, setup the microphone to copy to circualr buffer
         if let manager = self.audioManager,
             let fileReader = self.fileReader{
-            manager.inputBlock = self.handleMicrophone
-            manager.outputBlock = self.handleSpeakerQueryWithSinusoids
+            manager.inputBlock = self.handleMicrophone                   //setup microphone input blocks
+            manager.outputBlock = self.handleSpeakerQueryWithSinusoids   //setup speaker output blocks
             
-            // repeat this fps times per second using the timer class
-            //   every time this is called, we update the arrays "timeData" and "fftData"
+            // scheduled timer to update "timeData" and "fftData' at specific Fps
             Timer.scheduledTimer(withTimeInterval: 1.0/withFps, repeats: true) { _ in
                 self.runEveryInterval()
             }
             
+            // schedule timer to toggle "pulseValue" at specified intervals (every 1/5 = 0.2s) to create pulsing effect
+            // periodic change or oscillation in value to create visual effect in user interface
             Timer.scheduledTimer(withTimeInterval: 1.0/5.0, repeats: true) { _ in
-                // set to opposite
-                self.pulseValue += 1
+                
+                self.pulseValue += 1     //increase "pulseValue" by 1 each time the timer fires
+                
+                //check to see if "pulseValue" is >5
                 if self.pulseValue > 5{
-                    self.pulseValue = 0
+                    self.pulseValue = 0   //keeping pulseValue 0-5
                 }
             }
-            
-            fileReader.play()
-            
+            fileReader.play()   //start audio file playback
         }
     }
     
     
-    // You must call this when you want the audio to start being handled by our model
+    // method to start audio playback using audioManager
     func play(){
         if let manager = self.audioManager{
             manager.play()
         }
     }
     
+    
+    // method to stop audio playback using audioManager
     func stop(){
+        
+        // if audioManager is availabel
         if let manager = self.audioManager{
-            manager.pause()
+            manager.pause()            //pause method on manager to pause the audio processing
+            
+            //set these properties of manager to nil to stop audio input & output processing
             manager.inputBlock = nil
             manager.outputBlock = nil
         }
         
+        // if fileReader is available
         if let file = self.fileReader{
-            file.pause()
-            file.stop()
+            file.pause()  //pause file reading or playback
+            file.stop()   //stop file reading or playback
         }
         
+        // if there is inputBuffer available
         if let buffer = self.inputBuffer{
-            buffer.clear() // just makes zeros
+            buffer.clear()  //clear the buffer to reset the buffer
         }
+        
+        // to release resources associated with these objects as they are no longer needed
         inputBuffer = nil
         fftHelper = nil
         fileReader = nil
@@ -101,22 +124,29 @@ class AudioModel {
     
     //==========================================
     // MARK: Private Properties
+    
+    // to manager audio input and output
     private lazy var audioManager:Novocaine? = {
         return Novocaine.audioManager()
     }()
     
+    // performs FFT calculations
     private lazy var fftHelper:FFTHelper? = {
         return FFTHelper.init(fftSize: Int32(BUFFER_SIZE))
     }()
     
     
+    // to store audio data in a circular buffer
     private lazy var inputBuffer:CircularBuffer? = {
         return CircularBuffer.init(numChannels: Int64(self.audioManager!.numInputChannels),
                                    andBufferSize: Int64(BUFFER_SIZE))
     }()
     
+    
+    // to read audio data from a file
     private lazy var fileReader:AudioFileReader? = {
-            // find song in the main Bundle
+            
+            // find song "satisfaction" in the main Bundle
             if let url = Bundle.main.url(forResource: "satisfaction", withExtension: "mp3"){
                 // if we could find the url for the song in main bundle, setup file reader
                 // the file reader is doing a lot here becasue its a decoder
@@ -138,89 +168,112 @@ class AudioModel {
     
     
     //==========================================
-    // MARK: Private Methods
-    // NONE for this model
-    
-    
-    //==========================================
     // MARK: Model Callback Methods
+    
+    
+    // method to process audio data from input data to perform FFT on them converting time-domain data to frequency-domain data
     private func runEveryInterval(){
+        
+        // if the input buffer is available
         if inputBuffer != nil {
-            // copy time data to swift array
-            self.inputBuffer!.fetchFreshData(&timeData, // copied into this array
-                                             withNumSamples: Int64(BUFFER_SIZE))
-            
-            // now take FFT
-            fftHelper!.performForwardFFT(withData: &timeData,
-                                         andCopydBMagnitudeToBuffer: &fftData) // fft result is copied into fftData array
-            
-            // at this point, we have saved the data to the arrays:
-            //   timeData: the raw audio samples
-            //   fftData:  the FFT of those same samples
-            // the user can now use these variables however they like
-            
+            self.inputBuffer!.fetchFreshData(&timeData, withNumSamples: Int64(BUFFER_SIZE))  //copy fresh time-domain data to "timeData" array
+            fftHelper!.performForwardFFT(withData: &timeData, andCopydBMagnitudeToBuffer: &fftData) // perform FFT on timeData and store them in fftData
         }
     }
     
+    
     //==========================================
     // MARK: Audiocard Callbacks
-    // in obj-C it was (^InputBlock)(float *data, UInt32 numFrames, UInt32 numChannels)
-    // and in swift this translates to:
+    
+    // handle audio data received from  mic using parameters data, numFrames, and numChannels
     private func handleMicrophone (data:Optional<UnsafeMutablePointer<Float>>, numFrames:UInt32, numChannels: UInt32) {
-        // copy samples from the microphone into circular buffer
-        self.inputBuffer?.addNewFloatData(data, withNumSamples: Int64(numFrames))
+        self.inputBuffer?.addNewFloatData(data, withNumSamples: Int64(numFrames))  // copy samples from the microphone into circular buffer
     }
     
+    
+    // handle audio data sent to the speaker. Adds synthesized sine wave signals to audio output ("data") & modulates them based on certain conditions
     private func handleSpeakerQueryWithSinusoids(data:Optional<UnsafeMutablePointer<Float>>, numFrames:UInt32, numChannels: UInt32){
-            if let arrayData = data, let manager = self.audioManager{
+            
+        
+        // if output audio data is "data" and audioManger is "manager"
+        if let arrayData = data, let manager = self.audioManager{
                 
-                var addFreq:Float = 0
-                var mult:Float = 1.0
-                if pulsing && pulseValue==1{
-                    addFreq = 1000.0
+                var addFreq:Float = 0  // additional frequency variable (additional frequency component added to base frequency)
+                var mult:Float = 1.0   // multiplication factor variable
+            
+                // if pulsing is active & pulseValue is 1
+            if pulsing && pulseValue==1{
+                addFreq = 1000.0
+                // if pulsing is active & pulseValue is > 1
                 }else if pulsing && pulseValue > 1{
                     mult = 0.0
                 }
                 
+                // if there is a file reader
                 if let file = self.fileReader{
-                    // get samples from audio file, pass array by reference
+                    
+                    // get fresh samples from audio file & store them in "arrayData" array by reference
                     file.retrieveFreshAudio(arrayData,
                                 numFrames: numFrames,
                                 numChannels: numChannels)
                     
                     // adjust volume of audio file output
-                    var volume:Float = mult*0.15 // zero out song also, if needed
-                    vDSP_vsmul(arrayData, 1, &(volume), arrayData, 1, vDSP_Length(numFrames*numChannels))
-                                    
+                    var volume:Float = mult*0.15
+                    
+                    vDSP_vsmul(arrayData, 1, &(volume), arrayData, 1, vDSP_Length(numFrames*numChannels))  //multiply each element in "arrayData" by volume
                 }
                 
+            
+                // compute phase increments per sample for generating sine waves at specified frequency
                 phaseIncrement1 = Float(2*Double.pi*Double(sineFrequency1+addFreq)/manager.samplingRate)
                 phaseIncrement2 = Float(2*Double.pi*Double(sineFrequency2+addFreq)/manager.samplingRate)
                 phaseIncrement3 = Float(2*Double.pi*Double(sineFrequency3+addFreq)/manager.samplingRate)
                 
-                
-                var i = 0
-                let chan = Int(numChannels)
-                let frame = Int(numFrames)
+                // to generate sine waves and add audio data
+                var i = 0     // loop counter
+                let chan = Int(numChannels)   // number of channels
+                let frame = Int(numFrames)    // number of frames
+            
+                // if the audio is mono or single channel
                 if chan==1{
+                    
+                    //loop throgh eah frame
                     while i<frame{
+                        
+                        // add to arrayData: sum of three sine waves that are moduleted by corresponding phases & amplitudes
                         arrayData[i] += (0.9*sin(phase1)+0.4*sin(phase2)+0.1*sin(phase3))*mult
+                        
+                        // increment of phases
                         phase1 += phaseIncrement1
                         phase2 += phaseIncrement2
                         phase3 += phaseIncrement3
+                        
+                        // if a phase value exceeds "sineWaveRepeatMax", repeat next cycle since sine wave completed one cycle
                         if (phase1 >= sineWaveRepeatMax) { phase1 -= sineWaveRepeatMax }
                         if (phase2 >= sineWaveRepeatMax) { phase2 -= sineWaveRepeatMax }
                         if (phase3 >= sineWaveRepeatMax) { phase3 -= sineWaveRepeatMax }
                         i+=1
                     }
+                    
+                // if the aduio is
                 }else if chan==2{
+                    
+                    // length is frames X channels
                     let len = frame*chan
+                    
+                    // if i is less than length
                     while i<len{
+                        
+                        // add to arrayData: sum of three sine waves that are moduleted by corresponding phases & amplitudes
                         arrayData[i] += (0.9*sin(phase1)+0.4*sin(phase2)+0.1*sin(phase3))*mult
                         arrayData[i+1] = arrayData[i]
+                        
+                        // increment of phases
                         phase1 += phaseIncrement1
                         phase2 += phaseIncrement2
                         phase3 += phaseIncrement3
+                        
+                        // if a phase value exceeds "sineWaveRepeatMax", repeat next cycle since sine wave completed one cycle
                         if (phase1 >= sineWaveRepeatMax) { phase1 -= sineWaveRepeatMax }
                         if (phase2 >= sineWaveRepeatMax) { phase2 -= sineWaveRepeatMax }
                         if (phase3 >= sineWaveRepeatMax) { phase3 -= sineWaveRepeatMax }
@@ -230,6 +283,5 @@ class AudioModel {
                 
             }
         }
-    
     
 }
--- AudioLabSwift-7_AudioTestApp/AudioLabSwift/ViewController.swift
+++ AudioLabSwift-7_AudioTestApp/AudioLabSwift/ViewController.swift
@@ -1,32 +1,32 @@
-//
-//  ViewController.swift
-//  AudioLabSwift
-//
+
 //  Created by Eric Larson 
 //  Copyright © 2020 Eric Larson. All rights reserved.
-//
+
 
 import UIKit
 import Metal
 
 
-
-
-
 class ViewController: UIViewController {
 
-    @IBOutlet weak var userView: UIView!
+    @IBOutlet weak var userView: UIView!  // to display graph
+    
+    
+    // to hold constant buffer size = 4096
     struct AudioConstants{
         static let AUDIO_BUFFER_SIZE = 1024*4
     }
     
-    // setup audio model
-    let audio = AudioModel(buffer_size: AudioConstants.AUDIO_BUFFER_SIZE)
+    let audio = AudioModel(buffer_size: AudioConstants.AUDIO_BUFFER_SIZE)   // setup audio model with the buffer size
+    
+    // to display metal graph
     lazy var graph:MetalGraph? = {
         return MetalGraph(userView: self.userView)
     }()
-    var timer:Timer? = nil
     
+    var timer:Timer? = nil  // Timer property does not hold any value initially
+    
+    // initial value of frequency to be 300. "didSet" property observer updates the frequency, "labelFX" text when the frequency changes
     var frequency1:Float = 300 {
         didSet{
             audio.sineFrequency1 = frequency1
@@ -46,10 +46,13 @@ class ViewController: UIViewController {
         }
     }
     
+    
+    // to display frequency1, frequency2 and frequency3
     @IBOutlet weak var labelF1: UILabel!
     @IBOutlet weak var labelF2: UILabel!
     @IBOutlet weak var labelF3: UILabel!
     
+    // action for setting frequency2 close to frequency1 based on the given conditions
     @IBAction func setClose(_ sender: Any) {
         let diff = abs(frequency1-frequency2)
         if diff > 100 {
@@ -63,111 +66,93 @@ class ViewController: UIViewController {
         }
     }
     
+    // action to toggle the pulsing state in audio model based on switch's state
     @IBAction func shouldPulse(_ sender: UISwitch) {
         audio.pulsing = sender.isOn
     }
     
+    // actions to update frequency1, frequency2, and frequency3 based on the slider values
     @IBAction func sliderF1(_ sender: UISlider) {
         frequency1 = sender.value
     }
-    
     @IBAction func sliderF2(_ sender: UISlider) {
         frequency2 = sender.value
     }
-    
     @IBAction func sliderF3(_ sender: UISlider) {
         frequency3 = sender.value
     }
     
+    // set the initial values of the frequencies
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         frequency1 = 18000
         frequency2 = 18500
         frequency3 = 19000
-       
     }
     
+    
+    // call before the view becomes visible
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        // configure graph if available
         if let graph = self.graph{
-            graph.setBackgroundColor(r: 0, g: 0, b: 0, a: 1)
-            
-            // add in graphs for display
-            // note that we need to normalize the scale of this graph
-            // because the fft is returned in dB which has very large negative values and some large positive values
-            
-            // BONUS: lets also display a version of the FFT that is zoomed in
-            graph.addGraph(withName: "fftZoomed",
-                            shouldNormalizeForFFT: true,
-                            numPointsInGraph: 300) // 300 points to display
-            
-            
-            graph.addGraph(withName: "fft",
-                            shouldNormalizeForFFT: true,
-                            numPointsInGraph: AudioConstants.AUDIO_BUFFER_SIZE/2)
-            
-            graph.addGraph(withName: "time",
-                numPointsInGraph: AudioConstants.AUDIO_BUFFER_SIZE)
-            
-            
-            
-            graph.makeGrids() // add grids to graph
+            graph.setBackgroundColor(r: 0, g: 0, b: 0, a: 1)   //set background color to be black
+            graph.addGraph(withName: "time", numPointsInGraph: AudioConstants.AUDIO_BUFFER_SIZE)  //add time-domain graph
+            graph.addGraph(withName: "fft", shouldNormalizeForFFT: true, numPointsInGraph: AudioConstants.AUDIO_BUFFER_SIZE/2)  //add main fft graph
+            graph.addGraph(withName: "fftZoomed", shouldNormalizeForFFT: true, numPointsInGraph: 300) // add zoomed fft graph. 300 points to display
+            graph.makeGrids()   // add grids to graph
         }
-        
-        // start up the audio model here, querying microphone
-        audio.startMicrophoneProcessing(withFps: 20) // preferred number of FFT calculations per second
 
-        audio.play()
+        audio.startMicrophoneProcessing(withFps: 20)  // start audio processing at 20 Fps
+        audio.play()   // start audio playback
         
-        // run the loop for updating the graph peridocially
+        // run the loop for updating the graph peridocially (every 0.05 s)
         timer = Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true) { _ in
             self.updateGraph()
         }
     }
     
+    
+    // call after view disappears
     override func viewDidDisappear(_ animated: Bool) {
-        
-        timer?.invalidate()
-        graph?.teardown()
-        graph = nil
-        audio.stop()
+        timer?.invalidate()   //invalidate the timer
+        graph?.teardown()     // teardown the graph
+        graph = nil           //set the graph to nil
+        audio.stop()          // stop audio processing
         super.viewDidDisappear(animated)
-        
     }
     
-    // periodically, update the graph with refreshed FFT Data
+    
+    // periodically, update the graph with refreshed time-domain data , FFT Data
     func updateGraph(){
         
+        // if graph is available
         if let graph = self.graph{
-            graph.updateGraph(
-                data: self.audio.fftData,
-                forKey: "fft"
-            )
             
+            // update the time graph
             graph.updateGraph(
                 data: self.audio.timeData,
                 forKey: "time"
             )
             
-            // BONUS: show the zoomed FFT
-            // we can start at about 150Hz and show the next 300 points
-            // actual Hz = f_0 * N/F_s
-            let minfreq = min(min(frequency1,frequency2),frequency3)
-            let startIdx:Int = (Int(minfreq)-50) * AudioConstants.AUDIO_BUFFER_SIZE/audio.samplingRate
-            let subArray:[Float] = Array(self.audio.fftData[startIdx...startIdx+300])
+            //update the fft graph
+            graph.updateGraph(
+                data: self.audio.fftData,
+                forKey: "fft"
+            )
+      
+            let minfreq = min(min(frequency1,frequency2),frequency3)   // find min frequency from all three frequencies
+            let startIdx:Int = (Int(minfreq)-50) * AudioConstants.AUDIO_BUFFER_SIZE/audio.samplingRate  // determine starting index for fft data to be zoomed in
+            let subArray:[Float] = Array(self.audio.fftData[startIdx...startIdx+300])  //subarray to reprsent zoomed-in of the fft data
+            
+            // update fftZoomed graph
             graph.updateGraph(
                 data: subArray,
                 forKey: "fftZoomed"
             )
-            
-            
         }
-        
     }
-    
-    
 
 }
 
