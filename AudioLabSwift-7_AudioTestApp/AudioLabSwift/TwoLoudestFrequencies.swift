83e03bc5203425988dad2d28068cb533c177628c
Jeevan Rai <yamphurai007@yahoo.com>
2024-10-04 20:43:17 -0500

    Completed Module A but the app is not recognizing any of the two loudest frequencies.

---
+++ AudioLabSwift-7_AudioTestApp/AudioLabSwift/TwoLoudestFrequencies.swift
@@ -0,0 +1,30 @@
+//
+//  TwoLoudestFrequencies.swift
+//  AudioLabSwift
+//
+//  Created by Reshma Shrestha on 10/4/24.
+//  Copyright © 2024 Eric Larson. All rights reserved.
+//
+
+import UIKit
+
+class TwoLoudestFrequencies: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
