ae921789a9ee3b355fbb0117b50d6ce1af3ca01c
Jeevan Rai <yamphurai007@yahoo.com>
2024-10-10 22:34:33 -0500

    Added Clif section for module B.

---
+++ AudioLabSwift-7_AudioTestApp/AudioLabSwift/DopplerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DopplerViewController.swift
+//  AudioLabSwift
+//
+//  Created by Reshma Shrestha on 10/10/24.
+//  Copyright © 2024 Eric Larson. All rights reserved.
+//
+
+import UIKit
+
+class DopplerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
